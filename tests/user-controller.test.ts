708d8dbd2f7cdd1f27be653ae50c678eadf92796
Dmitri <dima337dk@gmail.com>
2025-10-25 09:48:12 +0300

    Homework 13

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -1,6 +1,7 @@
 // tests/api.spec.ts
 import { test, expect } from '@playwright/test';
 import {UserDTO} from "./DTO/UserDTO";
+import {StatusCodes} from "http-status-codes";
 
 let baseURLWithEndpoint: string = 'http://localhost:3000/users';
 
@@ -8,7 +9,7 @@ test.describe('User management API', () => {
 
     test('all users: should return empty array when no users', async ({ request }) => {
         const response = await request.get(`${baseURLWithEndpoint}`);
-        expect(response.status()).toBe(200);
+        expect(response.status()).toBe(StatusCodes.OK);
         const responseBody = await response.text()
         expect(responseBody).toBe('[]');
     });
@@ -17,19 +18,19 @@ test.describe('User management API', () => {
         const createUserResponse = await request.post(`${baseURLWithEndpoint}`);
         const createUserJson: UserDTO = await createUserResponse.json();
         const findUserResponse = await request.get(`${baseURLWithEndpoint}/${createUserJson.id}`);
-        expect(findUserResponse.status()).toBe(200);
+        expect(findUserResponse.status()).toBe(StatusCodes.OK);
         const findUserJson: UserDTO = await findUserResponse.json();
         UserDTO.checkServerResponse(findUserJson);
     });
 
     test('find user: should return 404 if user not found', async ({ request }) => {
         const findUserResponse = await request.get(`${baseURLWithEndpoint}/200`);
-        expect(findUserResponse.status()).toBe(404);
+        expect(findUserResponse.status()).toBe(StatusCodes.NOT_FOUND);
     });
 
     test('create user: should add a new user', async ({ request }) => {
         const createUserResponse = await request.post(`${baseURLWithEndpoint}`);
-        expect(createUserResponse.status()).toBe(201);
+        expect(createUserResponse.status()).toBe(StatusCodes.CREATED);
         const createUserJson: UserDTO = await createUserResponse.json();
         UserDTO.checkServerResponse(createUserJson);
     });
@@ -38,13 +39,13 @@ test.describe('User management API', () => {
         const createUserResponse = await request.post(`${baseURLWithEndpoint}`);
         const createUserJson: UserDTO = await createUserResponse.json();
         const deleteUserResponse = await request.delete(`${baseURLWithEndpoint}/${createUserJson.id}`);
-        expect(deleteUserResponse.status()).toBe(200);
+        expect(deleteUserResponse.status()).toBe(StatusCodes.OK);
         const deleteUserJson: UserDTO = await deleteUserResponse.json();
         UserDTO.checkServerResponse(deleteUserJson);
     });
 
     test('delete user: should return 404 if user not found', async ({ request }) => {
         const deleteUserResponse = await request.delete(`${baseURLWithEndpoint}/200`);
-        expect(deleteUserResponse.status()).toBe(404);
+        expect(deleteUserResponse.status()).toBe(StatusCodes.NOT_FOUND);
     });
 });
